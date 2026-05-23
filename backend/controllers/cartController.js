831a51ff8e648436fea2d2a196fdebea9a675a21
kwankhwanjai <himari.oork@gmail.com>
2026-05-23 22:58:03 +0700

    prepare backend structure for cart feature

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,7 @@
+//add products to user cart
+const addTocart = async (req, res) => {};
+//update  user cart
+const updateCart = async (req, res) => {};
+//get  user cart date
+const getUserCart = async (req, res) => {};
+export { addTocart, updateCart, getUserCart };
