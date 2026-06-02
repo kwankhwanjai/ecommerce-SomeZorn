925eccd0cced74717e7e46303c73b5d9dbd48223
kwankhwanjai <himari.oork@gmail.com>
2026-06-02 23:50:58 +0700

    feat: backend e-commerce core (auth, product, cart APIs)

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoutes.js";
 import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
 
 console.log("MONGO URI:", process.env.MONGODB_URI);
 
@@ -22,6 +23,7 @@ app.use(cors());
 // api endpoints
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
